2211da829dfb60617c1dfd91bbcecdc96ac71305
Ihor <ihorfront88@gmail.com>
2022-08-12 18:51:45 +0300

    add 1-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+btnStartRef.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  btnStartRef.disabled = true;
+  btnStopRef.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStopRef.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStopRef.disabled = true;
+  btnStartRef.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
